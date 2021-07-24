3b7ec8b2aae764814bdd92f841079fe24d3e4f6a
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-07-24 16:22:24 +0530

    plane and stars

--- reactfibre/src/App.js
+++ reactfibre/src/App.js
@@ -1,26 +1,38 @@
 import React from "react";
 import {Canvas} from '@react-three/fiber';
-import {OrbitControls} from '@react-three/drei';
+import {OrbitControls,Stars} from '@react-three/drei';
 
 import './App.css';
 
 function Box(){
   return(
-    <mesh>
+    <mesh position={[0,0,0]}>
       <boxBufferGeometry attach="geometry"/>
       <meshLambertMaterial attach="material" color="hotpink"/>
     </mesh>
   )
 }
 
+function Plane(){
+  return(
+    <mesh position={[0,0,0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100,100]}/>
+      <meshLambertMaterial attach="material" color="seafoamgreen"/>
+    </mesh>
+  )
+}
+
+
+
 function App() {
   return (
    <Canvas>
    <OrbitControls/>
+   <Stars/>
    <ambientLight intensity={0.5}/>
    <spotLight position={[10,15,10]} angle={0.3}/>
-
      <Box/>
+     <Plane/>
    </Canvas>
   );
 }
