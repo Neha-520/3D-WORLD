3f57b4c8e50745cb3d30156aecfbc4de08d1200c
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-08-26 19:29:53 +0530

    skybox final commit

--- skybox/src/App.js
+++ skybox/src/App.js
@@ -1,7 +1,27 @@
-import React, { Suspense } from 'react';
-import { Canvas, useThree } from '@react-three/fiber';
-import { CubeTextureLoader } from 'three';
+import React, { Suspense, useRef } from 'react';
+import { Canvas, extend, useFrame, useThree } from '@react-three/fiber';
+import { CubeCamera, CubeTextureLoader, RGBFormat, WebGLCubeRenderTarget } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import './App.css';
+extend({ OrbitControls })
+
+const CameraControls = () => {
+  const {
+    camera,
+    gl: { domElement }
+  } = useThree();
+
+  const controls = useRef();
+  useFrame(() => controls.current.update());
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      autoRotate
+      enableZoom={false}
+    />
+  )
+}
 
 
 function Skybox() {
@@ -18,18 +38,35 @@ function Skybox() {
   ]);
   //images are passed in array for 6 sides of cube
 
-  scene.background = texture;
+  scene.background = texture; //set scene bg prop to resulting texture
   return null;
 }
 
 function Sphere() {
   const { scene, gl } = useThree();
 
+  //used by CubeCamera as its webGLRenderTarget. parameters >>size in pixels and object with texture parameters 
+  const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
+    format: RGBFormat,
+    // generateMipmaps: true, 
+    // minFilter: LinearMipMapLinearFilter 
+  });
+  // used to generate texture for reflective sphere 
+  // must be updated on each frame in order to track camera movement
+
+  const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget); //contains 6 perspective cameras
+  cubeCamera.position.set(0, 0, 0);
+  scene.add(cubeCamera);
+
+  //update the cubecamera with current renderer and scene
+  useFrame(() => cubeCamera.update(gl, scene));
+
   return (
     <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} castShadow>
       <sphereGeometry attach="geometry" args={[2, 32, 32]} />
       <meshBasicMaterial
         attach="material"
+        envMap={cubeCamera.renderTarget.texture}
         roughness={0.1}
         metalness={1}
       />
@@ -41,8 +78,11 @@ function App() {
   return (
     <Suspense fallback={<div>Loading....</div>}>
       <Canvas>
-        <Sphere />
-        <Skybox />
+        <Suspense fallback={null}>
+          <Sphere />
+          <Skybox />
+        </Suspense>
+        <CameraControls />
       </Canvas>
     </Suspense>
   );
